3fdc4d733b2c30ee130db8be659cc7f6f47454ca
unknown <karina.yukting.li@gmail.com>
2024-03-18 00:42:06 -0400

    Imported componets and pages to App.jsx and render and defined routes for header, navigation and footer

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+// Import the React library
+import React from "react";
+
+// Import necessary components and utilities from React Router
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Import components and pages
+import Header from "./components/Header";
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+
+// Define the main App component
+function App() {
+  // Return JSX for the entire app structure
+  return (
+    // Set up the Router for handling navigation
+    <Router>
+      {/* Render the Header component */}
+      <Header />
+      <hr className="content-divider" />
+      {/* Define routes for different pages using Routes */}
+      <Routes>
+        {/* Route for the AboutMe page */}
+        <Route path="/" element={<AboutMe />} />
+
+        {/* Route for the Portfolio page */}
+        <Route path="/portfolio" element={<Portfolio />} />
+
+        {/* Route for the Contact page */}
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      {/* Render the Footer component */}
+      <Footer />
+    </Router>
+  );
+}
+
+// Export the App component as the default export
+export default App;
